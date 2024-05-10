8451cdae937b09522f839f8a30aa7c43462b8a83
Londos <londos2021@gmail.com>
2024-05-10 18:12:46 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    Binary = 0
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("" + control.deviceSerialNumber() + " : " + Binary)
+    serial.writeString("" + control.deviceSerialNumber() + " : " + Binary)
+})
+input.onButtonPressed(Button.B, function () {
+    Binary = 1
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+})
+let Binary = 0
+radio.setGroup(142)
